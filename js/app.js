3338b8208c4fd353fd5b381900224ee7caa0022c
Vincenzo (Wilk) Ferrari <wilk3ert@gmail.com>
2014-11-18 19:30:22 +0100

    added angular dep

---
+++ js/app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular
+    .module('AmsterdamConf', [])
+    .config(['$interpolateProvider', function($interpolateProvider){
+        $interpolateProvider.startSymbol('[[').endSymbol(']]');
+    }])
+    .controller('TemplateCtrl', ['$scope', function ($scope) {
+        $scope.text = '';
+        $scope.templateSubmit = function () {
+            alert("text value is:" + $scope.text);
+        };
+    }])
+    .controller('DirectiveCtrl', ['$scope', function ($scope) {
+        $scope.tools = ['AngularJS', 'Go Lang', 'Dart'];
+        $scope.selection = $scope.tools[0];
+
+        $scope.name = 'Wilk';
+
+        $scope.articles = [
+            {title: 'Article A', body: 'Content of article A', notes: 'Notes of article A'},
+            {title: 'Article B', body: 'Content of article B', notes: 'Notes of article B'},
+            {title: 'Article C', body: 'Content of article C', notes: 'Notes of article C'}
+        ];
+    }]);
